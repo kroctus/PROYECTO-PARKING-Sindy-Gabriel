0831e3e019283a73e11dcd2bf999ffb32e61c8f0
GabrielPayano <gabriel21kun@gmail.com>
2019-05-14 13:53:51 +0200

    "Agregamos las clases DAO y la clase conexion"

---
+++ src/main/java/parking/Conexion.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parking;
+
+/**
+ *
+ * @author gabriel
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author J. Carlos F. Vico <jcarlosvico@maralboran.es>
+ */
+public class Conexion {
+
+    private static final String SERVIDOR = "jdbc:mysql://192.168.56.101:3306/";
+    private static final String NOMBRE_BASE_DATOS = "prueba";
+    private static final String USER = "gabriel";
+    private static final String PASS = "1234";
+
+    private static Connection instancia = null;
+
+    // Constructor privado no accesible desde otras clases
+    private Conexion() {
+
+    }
+
+    // Método de clase para acceder a la instancia del objeto Connection
+    public static Connection getInstance() {
+        // Si el objeto Connection no está creado, se crea
+        if (instancia == null) {
+            try {
+
+                // Se crea el objeto Connection	
+                instancia = DriverManager.getConnection(SERVIDOR + NOMBRE_BASE_DATOS, USER, PASS);
+
+                System.out.println("Conexión realizada con éxito.");
+
+            } catch (SQLException e) {
+
+                // Error en la conexión
+                System.out.println("Conexión fallida: " + e.getMessage());
+            }
+        }
+        return instancia;
+    }
+    
+}
