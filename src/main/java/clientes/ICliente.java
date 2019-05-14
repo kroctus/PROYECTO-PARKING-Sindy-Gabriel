c63f0a6405966ca8d0c019c99308ca9c204189fd
Sindy <syohanaferreira@gmail.com>
2019-05-14 14:17:51 +0200

    Añade la interfaz ICliente con los métodos: - public List<ClienteVO> getAll() - public ClienteVO findByPk(String matricula) - public int insertCliente(ClienteVO cliente) - public int insertCliente(List<ClienteVO> lista) - public int deleteCliente(ClienteVO c) - public int deleteCliente() - public int updateCliente(String matricula, ClienteVO nuevosDatos)

---
+++ src/main/java/clientes/ICliente.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientes;
+
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author sindy
+ */
+public interface ICliente {
+
+    // Método para obtener todos los registros de la tabla clientes
+    List<ClienteVO> getAll() throws SQLException;
+
+    // Método para obtener un registro a partir de la matricula (la pk)
+    ClienteVO findByPk(String matricula) throws SQLException;
+
+    // Método para insertar un cliente
+    public int insertCliente(ClienteVO cliente) throws SQLException;
+
+    // Método para insertar varios clientes
+    public int insertCliente(List<ClienteVO> lista) throws SQLException;
+
+    // Método para borrar un cliente
+    public int deleteCliente(ClienteVO c) throws SQLException;
+
+    // Método para borrar toda la tabla clientes
+    public int deleteCliente() throws SQLException;
+
+    // Método para modificar un cliente. Se modifica al cliente que tenga esa 
+    //matrícula con los nuevos datos que traiga la persona 'nuevosDatos'
+    public int updateCliente(String matricula, ClienteVO nuevosDatos) throws SQLException;
+}
