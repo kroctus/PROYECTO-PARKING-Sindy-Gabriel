b4c5c1bb1fb6a807e9b7eace3e1e11f01463cd45
Sindy <syohanaferreira@gmail.com>
2019-05-27 12:20:10 +0200

    Añade método mostrarEstadoPlazas().

--- src/main/java/parking/Admin_Sindy.java
+++ src/main/java/parking/Admin_Sindy.java
@@ -5,10 +5,34 @@
  */
 package parking;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import plazas.PlazaDAO;
+import plazas.PlazaVO;
+
 /**
  *
  * @author sindy
  */
 public class Admin_Sindy {
+
+    public static void mostarEstadoPlazas() throws SQLException {
+        PlazaDAO plazaDao = new PlazaDAO();
+        ArrayList<PlazaVO> listaPlazas = new ArrayList<>();
+        listaPlazas = (ArrayList<PlazaVO>) plazaDao.getAll();
+        String plazas = "";
+        int contador = 0;
+        for (PlazaVO listaPlaza : listaPlazas) {
+            if (contador % 2 == 0) {
+                plazas = plazas + " " + listaPlaza.getNumPlaza() + ":" + PlazaVO.obtenerEstado(listaPlaza) + "\n";
+            } else {
+                plazas = plazas + " " + listaPlaza.getNumPlaza() + ":" + PlazaVO.obtenerEstado(listaPlaza);
+            }
+            contador++;
+        }
+
+        JOptionPane.showMessageDialog(null, "Estados de las plazas:\n" + plazas);
+    }
 
 }
