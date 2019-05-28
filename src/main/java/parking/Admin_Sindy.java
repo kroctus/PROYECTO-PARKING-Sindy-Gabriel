3048050d5c0f8d4a817db11bfc4829f45fc9abed
Sindy <syohanaferreira@gmail.com>
2019-05-28 10:30:01 +0200

    Añade método abonosCaducadosMes(int mes)

--- src/main/java/parking/Admin_Sindy.java
+++ src/main/java/parking/Admin_Sindy.java
@@ -6,10 +6,13 @@
 package parking;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 import plazas.PlazaDAO;
 import plazas.PlazaVO;
+import reservas.ReservasDAO;
+import reservas.ReservasVO;
 
 /**
  *
@@ -33,6 +36,43 @@ public class Admin_Sindy {
         }
 
         JOptionPane.showMessageDialog(null, "Estados de las plazas:\n" + plazas);
+    }
+
+    public static void abonosCaducadosMes(int mes) throws SQLException {
+        String reserva = "Matricula : Plaza :    Pin      :  Fecha inicio :   Fecha fin \n";
+        ReservasDAO reservasDao = new ReservasDAO();
+        //Lista de todas las reservas que hay en la bbdd
+        ArrayList<ReservasVO> listaReservas = new ArrayList<>();
+        //Lista que almacena las reservas que van a caducar en el mes indicado
+        ArrayList<ReservasVO> reservasCaducidadP = new ArrayList<>();
+        listaReservas = (ArrayList<ReservasVO>) reservasDao.getAll();
+        //Recorremos la lista de las reservas que hay en la base de datos
+        for (ReservasVO listaReserva : listaReservas) {
+            //Si el mes de la fecha de fin de abono coincide con la fecha que indica el usuario y
+            //si el año en el que hacemos la consulta coincide con el año del fin de abono entonces añadimos 
+            //Esa reserva a la lsita que mostraremos por pantalla
+            if (listaReserva.getFecfinabono().getMonthValue() == mes
+                    && listaReserva.getFecfinabono().getYear() == LocalDate.now().getYear()) {
+                reservasCaducidadP.add(listaReserva);
+            }
+        }
+
+        if (reservasCaducidadP.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "No se ha encontrado ninguna reserva que caduque en el mes " + mes);
+        } else {
+            for (ReservasVO reservasVO : reservasCaducidadP) {
+
+                reserva = reserva + reservasVO.getMatricula()
+                        + "  :  " + reservasVO.getNumplaza() + "  :  "
+                        + reservasVO.getPin_fijo() + "  :  "
+                        + reservasVO.getFeciniabono() + "  :  "
+                        + reservasVO.getFecfinabono() + "\n";
+
+            }
+            JOptionPane.showMessageDialog(null, "Reservas que caducan en el mes de " + mes + " \n" + reserva);
+
+        }
+
     }
 
 }
