d00f41ef5b944d539ca32b32db3fb078b3589661
Sindy <syohanaferreira@gmail.com>
2019-05-29 12:22:49 +0200

    Añade clase Main

---
+++ src/main/java/parking/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parking;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import java.text.ParseException;
+import javax.swing.JOptionPane;
+import static parking.Menu.mostrarMenu;
+
+/**
+ *
+ * @author sindy
+ */
+public class Main {
+    public static void main(String[] args) throws SQLException, ParseException, FileNotFoundException {
+        try {
+            int respuesta;
+            do {
+                mostrarMenu();
+                //respuesta = JOptionPane.showInputDialog("¿Quieres salir del menú?");
+                respuesta = JOptionPane.showOptionDialog(null, "¿Quieres salir del menú?",
+                "Menú", JOptionPane.YES_NO_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE, null, new Object[]{
+                    "Si", "No"},
+                " ");
+            } while (respuesta == 1);
+
+        } catch (SQLException | ParseException | FileNotFoundException sql) {
+            System.out.println("Ha ocurrido un error");
+            System.out.println(sql.getMessage());
+        }
+    }
+}
