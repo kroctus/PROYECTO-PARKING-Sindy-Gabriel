e1643093789b52692d3a9f2a4f8b22f676a1f32e
SindyF <syohanaferreira@gmail.com>
2019-05-29 19:32:57 +0200

    Añade método mostrarFacturacionNoAbonado()

--- src/main/java/parking/Admin_Sindy.java
+++ src/main/java/parking/Admin_Sindy.java
@@ -397,4 +397,63 @@ public class Admin_Sindy {
         }
     }
 
+    public static void mostrarFacturacionNoAbonado() throws SQLException {
+        TicketsDAO daoTickets = new TicketsDAO();
+        ArrayList<TicketsVO> listaTickets = new ArrayList<>();
+        ArrayList<TicketsVO> listaFacturaTicket = new ArrayList<>();
+        String fechaIni, fechaFin, horaIni, horaFin;
+        String tickets = "";
+        double precioTotal = 0;
+        int contadorTickets = 0; 
+
+        JOptionPane.showMessageDialog(null, "A continuación se le solicitará "
+                + "dos fechas y dos horas\n para saber el importe de la"
+                + " ganancia entre dichos rangos.\n El formato será el siguiente:\n "
+                + "Hora -> hh:mm:ss\n Fecha -> AAAA-MM-DD");
+        fechaIni = JOptionPane.showInputDialog("Introduce la primera fecha");
+        horaIni = JOptionPane.showInputDialog("Introduce la primera hora");
+        fechaFin = JOptionPane.showInputDialog("Introduce la segunda fecha");
+        horaFin = JOptionPane.showInputDialog("Introduce la segunda hora");
+
+        //Pasamos el string a LocalDate dividiendo el tokens por el guion ("-").
+        String[] fechaI = fechaIni.split("-");
+        LocalDate fecha_ini = LocalDate.of(Integer.valueOf(fechaI[0]), Integer.valueOf(fechaI[1]), Integer.valueOf(fechaI[2]));
+
+        String[] fechaF = fechaFin.split("-");
+        LocalDate fecha_fin = LocalDate.of(Integer.valueOf(fechaF[0]), Integer.valueOf(fechaF[1]), Integer.valueOf(fechaF[2]));
+
+        //Pasamos el string a LocalTime dividiendo el tokens por los dos puntos (":").
+        String[] horaI = horaIni.split(":");
+        LocalTime hora_ini = LocalTime.of(Integer.valueOf(horaI[0]), Integer.valueOf(horaI[1]), Integer.valueOf(horaI[2]));
+
+        String[] horaF = horaFin.split(":");
+        LocalTime hora_fin = LocalTime.of(Integer.valueOf(horaF[0]), Integer.valueOf(horaF[1]), Integer.valueOf(horaF[2]));
+
+        listaTickets = (ArrayList<TicketsVO>) daoTickets.getAll();
+        for (TicketsVO ticketsVO : listaFacturaTicket) {
+            if (ticketsVO.getFecfinpin().isAfter(fecha_ini)
+                    && ticketsVO.getFecfinpin().isBefore(fecha_fin)
+                    && ticketsVO.getHorasalticket().isAfter(hora_ini)
+                    && ticketsVO.getHorasalticket().isBefore(hora_fin)) {
+                contadorTickets ++;
+                listaFacturaTicket.add(ticketsVO);
+                precioTotal = precioTotal + ticketsVO.getPrecio();
+            }
+        }
+
+        if (listaFacturaTicket.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "El importe de la ganancia es 0 ya"
+                    + " que no\n se ha encontrado ningun cobo entre esas dos fechas");
+        } else {
+            for (TicketsVO ticketsVO : listaFacturaTicket) {
+                tickets = tickets + ticketsVO + "\n";
+            }
+            
+            JOptionPane.showMessageDialog(null, "El número de cobros que se han realizado entre " + fechaIni + " " + horaIni
+                    + " y " + fechaFin + " "+ horaFin+" es igual a: "
+                    + contadorTickets + "\n" + tickets + "\n Ganacias: "
+                    + precioTotal);
+        }
+    }
+
 }
